0a83efba34a9e5e5e4bf5cbb810ec749fed1a134
seabbs <s.e.abbott12@gmail.com>
2020-04-25 22:43:04 +0000

    testing comlete

--- update_nowcasts.R
+++ update_nowcasts.R
@@ -1,17 +1,17 @@
 
 # Packages -----------------------------------------------------------------
-require(EpiNow)
-require(NCoVUtils)
-require(furrr)
-require(future)
-require(dplyr)
-require(tidyr)
-require(magrittr)
-require(future.apply)
-require(fable)
-require(fabletools)
-require(feasts)
-require(urca)
+require(EpiNow, quietly = TRUE)
+require(NCoVUtils, quietly = TRUE)
+require(furrr, quietly = TRUE)
+require(future, quietly = TRUE)
+require(dplyr, quietly = TRUE)
+require(tidyr, quietly = TRUE)
+require(magrittr, quietly = TRUE)
+require(future.apply, quietly = TRUE)
+require(fable, quietly = TRUE)
+require(fabletools, quietly = TRUE)
+require(feasts, quietly = TRUE)
+require(urca, quietly = TRUE)
 
 
 
@@ -26,11 +26,12 @@ cases <- cases %>%
   dplyr::rename(local = cases) %>%
   dplyr::mutate(imported = 0) %>%
   tidyr::gather(key = "import_status", value = "cases", local, imported) %>% 
-  tidyr::drop_na(region)
+  tidyr::drop_na(region) %>% 
+  dplyr::filter(!region %in% "Faroe Islands")
 
 # Get linelist ------------------------------------------------------------
 
-linelist <-  NCoVUtils::get_international_linelist()
+linelist <-  NCoVUtils::get_international_linelist() 
 
 # Set up cores -----------------------------------------------------
 
@@ -51,8 +52,7 @@ EpiNow::regional_rt_pipeline(
     EpiSoon::fable_model(model = fabletools::combination_model(fable::RW(y ~ drift()), fable::ETS(y), 
                                                                fable::NAIVE(y),
                                                                cmbn_args = list(weights = "inv_var")), ...)
-  },
-  samples = 10
+  }
 )
 
 
